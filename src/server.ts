d4788d4027bfaafe6640b6fbb5594df4805d22c6
pedrolucaspalma <pedrolucaspalma@gmail.com>
2021-08-14 08:01:30 -0300

    add 1st route (GET route)

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,23 @@ import express from "express"
 
 const app = express();
 
-// http://localhost:3000
-app.listen(3000, () => console.log("server is running"));
+/*
 
+GET     => Buscar uma informação
+POST    => Inserir (Criar) uma informação
+PUT     => Alterar uma informação
+DELETE  => Remover uma informação
+PATCH   => Alterar uma informação específica
+
+*/
+
+
+app.get("/test", (request, response) => {
+    // Request  => Entrando
+    // Response => Saindo
+
+    return response.send("Olá NLW");
+})
+
+// http://localhost:3000
+app.listen(3000, () => console.log("server is running NLW"));
