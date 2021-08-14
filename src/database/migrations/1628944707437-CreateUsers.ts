e54bf554367a40db9531f897840d1b3064172ac9
pedrolucaspalma <pedrolucaspalma@gmail.com>
2021-08-14 09:38:44 -0300

    configuring migrations

---
+++ src/database/migrations/1628944707437-CreateUsers.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateUsers1628944707437 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
