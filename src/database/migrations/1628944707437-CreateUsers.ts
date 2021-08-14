def065c32c77219e52e8b9f97ee0290dbb49a7bc
pedrolucaspalma <pedrolucaspalma@gmail.com>
2021-08-14 09:54:49 -0300

    edit 1st migration. Create table users

--- src/database/migrations/1628944707437-CreateUsers.ts
+++ src/database/migrations/1628944707437-CreateUsers.ts
@@ -1,8 +1,43 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
 export class CreateUsers1628944707437 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "users",
+                columns:[
+                    {
+                        name:"id",
+                        type:"uuid",
+                        isPrimary: true
+                    },
+                    {
+                        name: "name",
+                        type: "varchar",
+                    },
+                    {
+                        name: "email",
+                        type: "varchar",
+                    },
+                    {
+                        name: "admin",
+                        type: "boolean",
+                        default: false,
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()",
+                    },
+                    {
+                        name: "updated_at",
+                        type: "timestamp",
+                        default: "now()",
+                    }
+                ]
+            })
+        )
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
