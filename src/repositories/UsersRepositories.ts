41df3254712bc053f3c74a8b8622ee88c4876ef1
pedrolucaspalma <pedrolucaspalma@gmail.com>
2021-08-14 11:46:05 -0300

    fix route add user

--- src/repositories/UsersRepositories.ts
+++ src/repositories/UsersRepositories.ts
@@ -1,8 +1,7 @@
 import { EntityRepository, Repository} from "typeorm";
 import { User } from "../entities/User";
 
-class UsersRepositories extends Repository<User> {
-
-}
+@EntityRepository(User)
+class UsersRepositories extends Repository<User> {}
 
 export { UsersRepositories }
