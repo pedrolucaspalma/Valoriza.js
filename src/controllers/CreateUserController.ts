0fc8341d7365c686be2d81cd39ee713dc2ae1488
pedrolucaspalma <pedrolucaspalma@gmail.com>
2021-08-15 11:11:55 -0300

    add CreateTag migration

--- src/controllers/CreateUserController.ts
+++ src/controllers/CreateUserController.ts
@@ -10,18 +10,7 @@ class CreateUserController{
         const user = await createUserService.execute({name, email, admin});
 
         return response.json(user);
-
-        // try{
-        //     const { name, email, admin } = request.body;
-
-        //     const createUserService = new CreateUserService();
-    
-        //     const user = await createUserService.execute({name, email, admin});
-    
-        //     return response.json(user);
-        // } catch(err) {
-        //     return response.status(400).json({error:err.message})
-        // }
+        
     }
 }
 
