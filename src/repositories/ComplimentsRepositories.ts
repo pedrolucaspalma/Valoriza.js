7db4ebece7e011f001820ebe84804d0386aafd3b
pedrolucaspalma <pedrolucaspalma@gmail.com>
2021-09-26 10:39:24 -0300

    change Compliments name to Compliment. Update Exports

--- src/repositories/ComplimentsRepositories.ts
+++ src/repositories/ComplimentsRepositories.ts
@@ -1,5 +1,5 @@
 import { Repository, EntityRepository } from "typeorm";
-import { Compliment } from "../entities/Compliments";
+import { Compliment } from "../entities/Compliment";
 
 @EntityRepository(Compliment)
 class ComplimentsRepositories extends Repository <Compliment> {
